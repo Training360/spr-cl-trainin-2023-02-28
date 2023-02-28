eedb8d2f4c74c8364e0b28290d41451106a67feb
Viczián István <viczian.istvan@gmail.com>
2023-02-28 16:40:51 +0100

    GraphQL Code-ból és naplózás

--- bff-service/src/main/java/frontendservice/service/CoursesService.java
+++ bff-service/src/main/java/frontendservice/service/CoursesService.java
@@ -2,12 +2,14 @@ package frontendservice.service;
 
 import frontendservice.coursegateway.CourseClient;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CoursesService {
 
     private CourseClient courseClient;
@@ -17,10 +19,12 @@ public class CoursesService {
     private CourseMapper courseMapper;
 
     public List<Course> findAllCourses() {
+        log.debug("findAllCourses");
         return courseMapper.toDto(courseClient.findAllCourses());
     }
 
     public CourseDetails findById(Long id) {
+        log.debug("findById");
         var course = courseClient.findCourseById(id);
         var employees = employeesService.listEmployees();
         return courseMapper.toDto(course, employees);
