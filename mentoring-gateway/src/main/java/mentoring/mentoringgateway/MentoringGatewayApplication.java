342cee918578ee6829e327dfa28d7f5de000577f
Viczián István <viczian.istvan@gmail.com>
2023-03-01 11:05:24 +0100

    Gateway kiindulási projekt

---
+++ mentoring-gateway/src/main/java/mentoring/mentoringgateway/MentoringGatewayApplication.java
@@ -0,0 +1,13 @@
+package mentoring.mentoringgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoringGatewayApplication.class, args);
+	}
+
+}
---
+++ mentoring-gateway/src/test/java/mentoring/mentoringgateway/MentoringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mentoring.mentoringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
